c13aa7ea61cfafe4fe014c2cf6df155b7c3557e3
valdrin001-source <valdrin.001@gmail.com>
2026-04-14 21:14:02 +0200

    anpassunmg

--- MusicDB Demo Starter/svelte.config.js
+++ MusicDB Demo Starter/svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-netlify'; 
- 
-export default { 
-    kit: { 
-        adapter: adapter({ 
-            edge: false, 
-            split: false 
-        }) 
-    } 
-}; 
+import adapter from '@sveltejs/adapter-netlify';
+
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
